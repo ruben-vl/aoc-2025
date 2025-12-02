c6ba0d211316eabbfdd82a7f6ae0525b899fec23
Ruben Van Laer <email@rubenvanlaer.com>
2025-12-02 18:41:35 +0100

    Add solution day 02 part 2

--- solutions/day02/solution.py
+++ solutions/day02/solution.py
@@ -10,6 +10,29 @@ def p1(input: list[tuple[int, int]]) -> int:
                 sum += n
     return sum
 
+def p2(input: list[tuple[int, int]]) -> int:
+    sum = 0
+    for start, end in input:
+        for n in range(start, end+1):
+            strn, lstrn = str(n), len(str(n))
+            reps = 2
+            while lstrn//reps >= 1:
+                parts = split_string(strn, reps)
+                if all(p == parts[0] for p in parts):
+                    sum += n
+                    break
+                reps += 1
+    return sum
+
+def split_string(string: str, parts: int) -> list[str]:
+    res: list[str] = []
+    pl = len(string)//parts
+    for p in range(parts-1):
+        res.append(string[p*pl:(p+1)*pl])
+    res.append(string[(parts-1)*pl:])
+    return res
+
 if __name__ == "__main__":
     input = parse_input("solutions/day02/input.txt")
     print(f"[P1] The sum of all invalid IDs is: {p1(input)}")
+    print(f"[P2] The sum of all invalid IDs is: {p2(input)}")
