2047aded9e89063a3d1da75f0104febca6dfd42c
Ruben Van Laer <email@rubenvanlaer.com>
2025-12-12 15:34:28 +0100

    Add solution day 06 part 1+2

--- solutions/day06/solution.py
+++ solutions/day06/solution.py
@@ -1,36 +1,19 @@
 import math
+from more_itertools import split_at
 
-def parse_input(file: str) -> list[list[int|str]]:
-    rows = [row.rstrip().split() for row in open(file)]
-    groups: list[list[int|str]] = []
-    for i in range(len(rows[0])):
-        group = [int(row[i]) if ri != len(rows)-1 else row[i] for ri, row in enumerate(rows)]
-        groups.append(group)
-    return groups
+def parse_horizontally(path: str) -> tuple[list[list[int]], list[str]]:
+    rows = [row.rstrip().split() for row in open(path)]
+    return [list(map(int, s)) for s in zip(*rows[:-1])], rows[-1]
 
-def p1(input: list[list[int|str]]) -> int:
-    result = 0
-    for group in input:
-        if group[-1] == "+":
-            result += sum([int(e) for e in group[:-1]])
-        if group[-1] == "*":
-            result += math.prod([int(e) for e in group[:-1]])
-    return result
+def parse_vertically(path: str) -> tuple[list[list[int]], list[str]]:
+    rows = [row.replace('\n','') for row in open(path)]
+    numstr = list(map(lambda x: "".join(x).replace(' ', ''), zip(*rows[:-1])))
+    return [list(map(int, e)) for e in split_at(numstr, lambda x: x == '')], rows[-1].split()
 
-def p2(input: list[list[int|str]]) -> int:
-    corrected_groups: list[list[int|str]] = []
-    for group in input:
-        actual_group: list[int|str] = []
-        str_nums = [str(e) for e in group[:-1]]
-        max_len = max([len(e) for e in str_nums])
-        for col in range(max_len-1, -1, -1):
-            num = "".join([num[col] for num in str_nums if len(num) > col])
-            actual_group.append(int(num))
-        corrected_groups.append(actual_group + [group[-1]])
-    print(corrected_groups)
-    return p1(corrected_groups)
+def add_worksheet(numbers: list[list[int]], operations: list[str]) -> int:
+    return sum([sum(numbers[i]) if operations[i] == '+' else math.prod(numbers[i]) 
+                for i in range(len(numbers))])
 
 if __name__ == "__main__":
-    input = parse_input("solutions/day06/test.txt")
-    print(f"[P1] The grand total is: {p1(input)}")
-    print(f"[P2] The grand total is: {p2(input)}")
+    print(f"[P1] The total is: {add_worksheet(*parse_horizontally("solutions/day06/input.txt"))}")
+    print(f"[P2] The total is: {add_worksheet(*parse_vertically("solutions/day06/input.txt"))}")
